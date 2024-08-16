e4c49afe56ba3aded04cc8e5ff62624e28dccfac
João Pedro Maciel <joao.maciel@intranet.local>
2024-08-16 14:58:47 -0300

    oitdbem

---
+++ dia1/app.js
@@ -0,0 +1,17 @@
+let n;
+n=10;
+const idade = 30;
+const peso = 84;
+const altura = 1.8;
+let imc = peso/Math.pow(1.8, 2);
+
+console.log('Luiz otávio miranda tem',idade,'anos, pesa',peso,'kgs, tem uma altura de',altura,'e um imc de',imc);
+
+
+
+console.log('oi td bem eu sou o "joao" e estou aprendendo js às '+ 10+' da manhã');
+console.log('oi td bem eu sou o "joao" e estou aprendendo js às',10,'da manhã');
+console.log('oi td bem eu sou o "joao" e estou aprendendo js às',n,'da manhã');
+
+
+// ctrl + ;  faz comentarios na linha selecionada.
---
+++ dia1/index.js
@@ -0,0 +1,9 @@
+let n;
+n=10;
+
+console.log('oi td bem eu sou o "joao" e estou aprendendo js às '+ 10+' da manhã');
+console.log('oi td bem eu sou o "joao" e estou aprendendo js às',10,'da manhã');
+console.log('oi td bem eu sou o "joao" e estou aprendendo js às',n,'da manhã');
+
+
+// ctrl + ;  faz comentarios na linha selecionada.
