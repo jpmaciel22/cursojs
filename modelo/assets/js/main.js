6e704882990122c61b4838e8a28b7367d6e28b5c
João Pedro Maciel <joao.maciel@intranet.local>
2024-09-04 14:16:32 -0300

    oi

--- modelo/assets/js/main.js
+++ modelo/assets/js/main.js
@@ -7,18 +7,61 @@ const inputPeso = e.target.querySelector('#peso'); // e.target pega como alvo a 
 // esta ocorrendo e selecionado
 const peso = Number(inputPeso.value)
 const altura = Number(inputAltura.value)
+
+if(!peso){
+    setResultado('Peso inválido', false);
+    return;
+}
+
+if(!altura){
+    setResultado('Altura inválida', false);
+    return;
+}
+
+const imc = getImc(peso, altura);
+const nivelImc = getNivelImc(imc);
+const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+setResultado(msg,true);
 })
+
+
+function getNivelImc(imc){
+const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade Grau 1', 'Obesidade Grau 2', 'Obesidade Grau 3']
+
+if(imc >= 39.9) return nivel[5];
+if(imc >= 34.9) return nivel[4];
+if(imc >= 29.9) return nivel[3];
+if(imc >= 24.9) return nivel[2];
+if(imc >= 18.5) return nivel[1];
+if(imc < 18.5) return nivel[0];
+
+}
+
+
+function getImc(peso, altura){
+const imc = peso / altura ** 2;
+return imc.toFixed(2); // coloca o float em 2 casas decimais
+
+}
+
+
+
+
 
 function criaP(){
     const p = document.createElement('p'); // cria na var p o elemento paragrafo de HTML
-    p.classList.add('paragrafo-resultado') // insere no paragrafo a classe 'paragrafo-resultado'
     return p;
 }
 
-function setResultado(msg){
+function setResultado(msg, isValid){
     const resultado = document.querySelector('.resultado');
     resultado.innerHTML = ''; // cria o elemento da string do resultado e zera ela
     const p = criaP();
+
+if (isValid) {p.classList.add('paragrafo-resultado');}
+else{p.classList.add('paragrafo-incorreto');}
+
+    p.innerHTML = msg; // insere em P o texto digitado pela var msg
     resultado.appendChild(p); // insere no elemento resultado o que estiver no paragrafo p
 
 }
--- modelo/assets/js/mainMinhaSolucao.js
+++ modelo/assets/js/mainMinhaSolucao.js
@@ -10,29 +10,30 @@ function funcaoDefault(){
         const altura = form.querySelector('#altura');
         const alturaToNumber = parseFloat(altura.value);
         let imc = pesoToNumber / (alturaToNumber * alturaToNumber);
+        imc = imc.toFixed(2);
 
         if(18,5 > imc ){
-        resultado.innerHTML =`<p>Você está abaixo do peso.</p>`
+        resultado.innerHTML =`<p>Seu imc é: ${imc}. Você está abaixo do peso.</p>`
         }
 
         else if(18,5 <= imc && imc < 25 ){
-            resultado.innerHTML =`<p>Você está com peso normal.</p>`
+            resultado.innerHTML =`<p>Seu imc é :${imc}. Você está com peso normal.</p>`
             }
         
         else if(25 <= imc && imc < 30 ){
-                resultado.innerHTML =`<p>Você está com sobrepeso.</p>`
+                resultado.innerHTML =`<p>Seu imc é: ${imc}. Você está com sobrepeso.</p>`
                 }
         else if(30 <= imc && imc < 34 ){
-                    resultado.innerHTML =`<p>Você está com obesidade grau 1.</p>`
+                    resultado.innerHTML =`<p>Seu imc é: ${imc}. Você está com obesidade grau 1.</p>`
                     }        
         else if(30 <= imc && imc < 34 ){
-                        resultado.innerHTML =`<p>Você está com obesidade grau 2.</p>`
+                        resultado.innerHTML =`<p>Seu imc é: ${imc}. Você está com obesidade grau 2.</p>`
                         } 
         else if(35 <= imc && imc < 40 ){
-                            resultado.innerHTML =`<p>Você está com obesidade grau 2.</p>`
+                            resultado.innerHTML =`<p>Seu imc é: ${imc}. Você está com obesidade grau 2.</p>`
                             } 
         else if(imc >= 40 ){
-                                resultado.innerHTML =`<p>Você está com obesidade grau 3.</p>`
+                                resultado.innerHTML =`<p>Seu imc é: ${imc}. Você está com obesidade grau 3.</p>`
                                 } 
         else{
             resultado.innerHTML =`<p>Peso e altura inválidos.</p>`
