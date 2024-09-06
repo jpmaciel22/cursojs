3aaf33350d7ee106ea4a5c2f9ec325b135194fce
João Pedro Maciel <joao.maciel@intranet.local>
2024-09-06 15:09:16 -0300

    oi tduo bem

--- secao3/dia8/forOf.js
+++ secao3/dia8/forOf.js
@@ -23,3 +23,22 @@ for ( let i = 0; i < carros.length ; i++){
 
     console.log(carros[i]);
 }
+
+console.log('################');
+
+carros.forEach(function(valor, indice){ //esta funcao é por default para ser usada assim.
+    console.log(valor, indice);
+});
+
+/***  portanto sao a mesma coisa:
+ * for ( let i = 0 ; i < carros.length ; i++){console.log(carros[i])}
+ * for (let i in carros){console.log(carros[i])}
+ * for (let i of carros){console.log(i)}
+ * 
+ */
+
+
+// For classico retorna tanto o valor quanto o indice e funciona apenas com arrays e strings
+//  for in funciona tanto com arrays e strings quanto com objetos e retorna os indices os parametros
+//  e os valores.
+//  for of utiliza-se para achar apenas o valor dos arrays e strings.
