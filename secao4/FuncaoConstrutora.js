f5d53d54cdeb8d8791e25d1946fb2801e17059aa
João Pedro Maciel <joao.maciel@intranet.local>
2024-09-13 12:19:50 -0300

    o.

--- secao4/FuncaoConstrutora.js
+++ secao4/FuncaoConstrutora.js
@@ -3,11 +3,19 @@
 // Construtora -- > Pessoa ( new )
 //convencao para funcoes construtoras é comecar com letra maiuscula
 function Pessoa(nome,sobrenome,x,y){
+
+// ------ Atributos Privados ------
+    const ID = 12312313; // isso é apenas interno nao sera usado externamente como pessoa1.ID;
+// assim como:
+const metodoInterno = function(){
+
+};
+// ------ Atributos Publicos ------
 this.nome = nome;
 this.sobrenome = sobrenome;
 this.soma = function(){
 
-}
+};
 }
 pessoa1 = new Pessoa('joao','maciel',1,2);
 
