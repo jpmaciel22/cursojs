70cae98d8cb4400f2374bff4489529587abbe669
João Pedro Maciel <joao.maciel@intranet.local>
2024-09-20 12:21:22 -0300

    CPF

---
+++ secao6/ValidandoCPF.js
@@ -0,0 +1,19 @@
+// 705.484.450-52
+/*
+7  0  5  4  8  4  4  5  0  
+10 9  8  7  6  5  4  3  2
+70 0  40 28 48 20 16 15 0 --> soma = 237
+
+formula: 11 - ( 237 % 11 ) = 5 (primeiro digito pós traço)
+Se o numero for maior que 9 consideramos 0.
+
+7  0  5  4  8  4  4  5  0  5
+11 10 9  8  7  6  5  4  3  2
+77 0  45 32 56 24 20 20 0 10 --> soma = 284 
+formula 11 - ( 284 % 11 ) = 2 ( segundo digito pós traço)
+
+705.484.450-52 === 705.484.450-52
+
+se variar algum número, o cpf não é valido.
+*/
+
