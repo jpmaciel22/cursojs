5b21efaff0139a902471237203002e152b98ce70
João Pedro Maciel <joao.maciel@intranet.local>
2024-10-23 14:54:12 -0300

    Write Read Promises

--- node/writingEReading/modules/escrever.js
+++ node/writingEReading/modules/escrever.js
@@ -6,3 +6,6 @@ fs.writeFile(caminhoArquivo, 'skdfjdfklgfdklgjdf\n', {flag: 'a', encoding: 'utf-
 // fs.writeFile(caminhoArquivo, 'skdfjdfklgfdklgjdf\n', {flag: 'w', encoding: 'utf-8'});
 
 // com a flag 'w' ele subscreve tudo o que já tinha escrito no arquivo antes, caso o mesmo já exista.
+fs.appendFile(caminhoArquivo, 'OiTudoBem?\n', (error) => {
+   if(error) console.log(error);
+})
