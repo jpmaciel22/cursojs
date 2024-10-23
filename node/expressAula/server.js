1d37d6a2e8ecee567766284cc4b3f159a164afc0
João Pedro Maciel <joao.maciel@intranet.local>
2024-10-23 16:29:07 -0300

    express

---
+++ node/expressAula/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+// CRUD = CREATE, READ, UPDATE, DELETE
+//        POST    GET   PUT     DELETE
+
+// https://postoldweb.neocites.org/ <- GET -> Entregue a página.
+app.get('/', (requisicao, resposta) => {
+    
+})
