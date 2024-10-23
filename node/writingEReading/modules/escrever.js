e2ffd78bc02322c84c901e35cf941a397dcdeaca
João Pedro Maciel <joao.maciel@intranet.local>
2024-10-23 14:51:41 -0300

    oi

--- node/writingEReading/modules/escrever.js
+++ node/writingEReading/modules/escrever.js
@@ -3,3 +3,6 @@ const path = require('path');
 const caminhoArquivo = path.resolve(__dirname, '..', 'teste.txt')
 
 fs.writeFile(caminhoArquivo, 'skdfjdfklgfdklgjdf\n', {flag: 'a', encoding: 'utf-8'});
+// fs.writeFile(caminhoArquivo, 'skdfjdfklgfdklgjdf\n', {flag: 'w', encoding: 'utf-8'});
+
+// com a flag 'w' ele subscreve tudo o que já tinha escrito no arquivo antes, caso o mesmo já exista.
--- node/writingEReading/modules/ler.js
+++ node/writingEReading/modules/ler.js
@@ -2,4 +2,22 @@ const fs = require('fs').promises;
 const path = require('path');
 const caminhoArquivo = path.resolve(__dirname, '..', 'teste.txt')
 
-const li = fs.readFile(caminhoArquivo);
+// fs.readFile(caminhoArquivo, 'utf-8', (error, data) => {
+//     if (error) {
+//         if (error.code === "ENOENT") {
+//             console.error("File not found:", err.path);
+//         } else {
+//             console.error("Error reading file:", err);
+//         }
+//         return;}
+//     console.log(data);
+// });
+
+
+ // o caso acima seria sem promises
+
+const li = fs.readFile(caminhoArquivo)
+.then((resultado) => {
+    console.log(resultado.toString()); // é necessário o toString pois por padrao esse buffer do readfiles
+    // retorna em binário
+})
