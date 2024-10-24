7e502e435a355b264c322af47b7e0cad976aad59
João Pedro Maciel <joao.maciel@intranet.local>
2024-10-24 14:08:01 -0300

    EXPRESS

--- node/expressAula2/server.js
+++ node/expressAula2/server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
 
-// CRUD = CREATE, READ, UPDATE, DELETE
-//        POST    GET   PUT     DELETE
-
-// https://postoldweb.neocites.org/ <- GET -> Entregue a página.
 app.get('/', (requisicao, resposta) => {
   resposta.send(`<form action="/" method="POST">
     Nome: <input type="text" name="nome">
@@ -15,11 +11,6 @@ app.get('/', (requisicao, resposta) => {
 app.post('/', (requisicao,resposta) => {
     resposta.send('Recebi o formulário.')
 })
-
-app.get('/contato',(requisicao, resposta) => {
-    resposta.send('Obrigado por entrar em contato')
-})
-
 
 app.listen(3000, () => {
     console.log('Servidor executando na porta 3000')
