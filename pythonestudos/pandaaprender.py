a7aa1a232b7e81808867441f386588cfaf0b683c
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2024-11-11 15:53:37 -0300

    kaggle pandas chapter 6

--- pythonestudos/pandaaprender.py
+++ pythonestudos/pandaaprender.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 BASEDEDADOS = pd.read_csv("./winemag-data-130k-v2.csv")
-print(BASEDEDADOS.head())
+# print(BASEDEDADOS.head())
 # print(BASEDEDADOS.loc[1:5,:]) # mostra os dados das linhas 1 a 5 com todas as colunas
 # print(BASEDEDADOS.loc[(BASEDEDADOS.Brand == 'Wei Lih') & (BASEDEDADOS.Stars >= '5')])
 # o .loc é utilizado para filtrar
@@ -11,11 +11,13 @@ print(BASEDEDADOS.head())
 # BASEDEDADOS.groupby('points').points.count()
 # BASEDEDADOS.groupby('points').price.min()
 
-BASEDEDADOS.groupby(['country', 'province']).apply(lambda df: df.loc[df.points.idxmax()]).reset_index(drop=True)
+# BASEDEDADOS.groupby(['country', 'province']).apply(lambda df: df.loc[df.points.idxmax()]).reset_index(drop=True)
 # o comando acima separa por grupos de paises e provincias, e nos dados grupos criados, tenta localizar
 # a partir do array o objeto que tiver o maixmo de pontos nesse indice ( pontos ), portanto
 # ele localiza o melhor vinho dentro de cada grupo de cada provincia e país. 
-
+countries_reviewed = BASEDEDADOS.groupby(['country', 'province']).description.agg([len])
+countries_reviewed = countries_reviewed.reset_index()
+print(countries_reviewed.sort_values(by='len'))
 # plt.plot(BASEDEDADOS.price, BASEDEDADOS.points);
 # plt.title('Relacao entre preço e pontos');
 # plt.xlabel('Price')
