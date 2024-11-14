ed3d592b030f58c7bdbcbe5b80db0e7a3b98d932
jpmaciel22 <jpbmaciel@gmail.com>
2024-11-14 18:29:15 -0300

    adicionado matplotlib gasolina

--- pythonestudos/pandaaprender.py
+++ pythonestudos/pandaaprender.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 BASEDEDADOS = pd.read_csv("./winemag-data-130k-v2.csv")
-print(BASEDEDADOS.head())
+# print(BASEDEDADOS.head())
 # print(BASEDEDADOS.loc[1:5,:]) # mostra os dados das linhas 1 a 5 com todas as colunas
 # print(BASEDEDADOS.loc[(BASEDEDADOS.Brand == 'Wei Lih') & (BASEDEDADOS.Stars >= '5')])
 # o .loc é utilizado para filtrar
@@ -21,5 +21,14 @@ BASEDEDADOS.groupby(['country', 'province']).apply(lambda df: df.loc[df.points.i
 # plt.xlabel('Price')
 # plt.ylabel('Points')
 # plt.show()
+
+#Maneira mais manual abaixo:
+# plt.plot(gas.Year, gas.USA, '.-', label="USA") # o b.- define cor e que tem pontos na linha..
+# b de blue e o . de o simbolo ponto... r>- faz um vermelho cheio de setas.. se mudarmos para r>: fica 
+# pontilhado etc...
+# plt.xticks(gas.Year[::2]) # aq é como o iloc, colocamos para pular de 2 em 2 anos.
+# plt.plot(gas['Year'], gas['Australia'], label="Australia") # you can do it both ways
+
+
 
 
